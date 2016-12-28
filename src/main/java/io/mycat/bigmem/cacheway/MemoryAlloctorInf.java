c6e384e71fbb63bb1d1c7761efd04cd418df5b3c
kkzfl <546064298@qq.com>
2016-12-28 17:54:20 +0800

    添加简单的内存分配工厂

--- src/main/java/io/mycat/bigmem/cacheway/MemoryAlloctorInf.java
+++ src/main/java/io/mycat/bigmem/cacheway/MemoryAlloctorInf.java
@@ -4,7 +4,7 @@ import io.mycat.bigmem.buffer.MycatBuffer;
 import io.mycat.bigmem.buffer.MycatBufferBase;
 
 /**
- * 用来进行缓存操作的接口
+ * 用来进行内存操作的接口
 * 源文件名：CacheOperatorInf.java
 * 文件版本：1.0.0
 * 创建作者：liujun
@@ -14,26 +14,17 @@ import io.mycat.bigmem.buffer.MycatBufferBase;
 * 文件描述：TODO
 * 版权所有：Copyright 2016 zjhz, Inc. All Rights Reserved.
 */
-public interface CacheOperatorInf {
+public interface MemoryAlloctorInf {
 
     /**
      * 进行缓存空间的分配
     * 方法描述
+    * @param allocFlag 按位来进行的
     * @param size 需要的内存大小
-    * @param timeout 过期的时间
     * @return
     * @创建日期 2016年12月20日
     */
-    public MycatBuffer allocationMemory(int size, long timeOut);
-
-    /**
-     * 进行缓存空间的全部释放
-    * 方法描述
-    * @param bufer
-    * @return
-    * @创建日期 2016年12月20日
-    */
-    public boolean recycleAll(MycatBufferBase bufer);
+    public MycatBufferBase allocMem(int allocFlag, int size);
 
     /**
      * 进行缓存空间的部分释放，即释放buffer的limit与capacity之间的空间释放
@@ -42,6 +33,6 @@ public interface CacheOperatorInf {
      * @return
      * @创建日期 2016年12月20日
      */
-    public boolean recycleNotUse(MycatBufferBase bufer);
+    public void recyleMem(MycatBufferBase bufer);
 
 }
